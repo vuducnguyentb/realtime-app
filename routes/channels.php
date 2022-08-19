cc9fe2be87e22af73f9af17c7afa09cf05028e2d
Vũ Đức Nguyên <nguyenvd@tcv.vn>
2022-08-19 17:13:28 +0700

    2. Managing the List of Connected Users in Realtime

--- routes/channels.php
+++ routes/channels.php
@@ -12,8 +12,15 @@
 */
 
 Broadcast::channel('App.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return (int)$user->id === (int)$id;
 });
+
 Broadcast::channel('notifications', function ($user) {
     return $user != null;
+});
+
+Broadcast::channel('chat', function ($user) {
+    if ($user != null) {
+        return ['id' => $user->id, 'name' => $user->name];
+    }
 });
